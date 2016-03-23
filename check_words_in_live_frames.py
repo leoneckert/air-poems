873909efe2a3194020d1a8b7810edc465f29edff
Leon Eckert <eckertleon@gmail.com>
2016-03-22 22:10:08 -0400

    better structure and more comments

--- check_words_in_live_frames.py
+++ check_words_in_live_frames.py
@@ -1,6 +1,24 @@
 import sys
 import time
 import random
+import subprocess
+
+
+                                          
+#   ___ ___  _ __ ___  _ __   __ _ _ __ ___ 
+#  / __/ _ \| '_ ` _ \| '_ \ / _` | '__/ _ \
+# | (_| (_) | | | | | | |_) | (_| | | |  __/
+#  \___\___/|_| |_| |_| .__/ \__,_|_|  \___|
+#                     | |                   
+#                     |_|          
+#                        _     
+#                       | |    
+# __      _____  _ __ __| |___ 
+# \ \ /\ / / _ \| '__/ _` / __|
+#  \ V  V / (_) | | | (_| \__ \
+#   \_/\_/ \___/|_|  \__,_|___/
+                             
+
 #------------------------------------------------------------
 # run time increases a lot with longer networknames (->perhaps only allow below a threshold)
 def check_if_part_of_word1_equals_word2(word1, word2, min_length):
@@ -18,7 +36,31 @@ def check_if_part_of_word1_equals_word2(word1, word2, min_length):
 				requiredLength = len(word2)
 				match = word2
 	return match
+# #------------------------------------------------------------
+
+#  _                   _             
+# | |                 (_)            
+# | | ___   ___  _ __  _ _ __   __ _ 
+# | |/ _ \ / _ \| '_ \| | '_ \ / _` |
+# | | (_) | (_) | |_) | | | | | (_| |
+# |_|\___/ \___/| .__/|_|_| |_|\__, |
+#               | |             __/ |
+#               |_|            |___/ 
+#      _ _      _   _                   _      _           
+#     | (_)    | | (_)                 (_)    (_)          
+#   __| |_  ___| |_ _  ___  _ __   __ _ _ _ __ _  ___  ___ 
+#  / _` | |/ __| __| |/ _ \| '_ \ / _` | | '__| |/ _ \/ __|
+# | (_| | | (__| |_| | (_) | | | | (_| | | |  | |  __/\__ \
+#  \__,_|_|\___|\__|_|\___/|_| |_|\__,_|_|_|  |_|\___||___/
+                                                         
+
 #------------------------------------------------------------
+# all dictionairies need to be named in plural (ending on "s", e.g. "peoples.txt").
+# the order in the next list matters: it will always take the longest corresponding word,
+# however, if two are the same length, the one from the earlier dictionairy will be taken
+dictionaries = [ "peoples", "nouns", "places"]
+
+
 
 # there can be more than one match, we want the longest one
 def check_for_words_in_network_name(_network_name):
@@ -27,108 +69,30 @@ def check_for_words_in_network_name(_network_name):
 	longestmatch.append("") #actual match
 	longestmatch.append("") #type
 
-	places = open('places.txt', 'r')
-	for place in places:
-		place = place.strip()
-		matchinplace = check_if_part_of_word1_equals_word2(networkname,place,len(longestmatch[0]))
-		if len(matchinplace) > 0:
-			longestmatch[0] = matchinplace
-			longestmatch[1] = "place"
-
-	peoples = open('people.txt', 'r')
-	for people in peoples:
-		people = people.strip()
-		matchingpeople = check_if_part_of_word1_equals_word2(networkname,people,len(longestmatch[0]))
-		if len(matchingpeople) > 0:
-			longestmatch[0] = matchingpeople
-			longestmatch[1] = "people"
-
-	nouns = open('91Knouns.txt', 'r')
-	for noun in nouns:
-		noun = noun.strip()
-		matchingnoun = check_if_part_of_word1_equals_word2(networkname,noun,len(longestmatch[0]))
-		if len(matchingnoun) > 0:
-			longestmatch[0] = matchingnoun
-			longestmatch[1] = "noun"
-
-	verbs = open('31Kverbs.txt', 'r')
-	for verb in verbs:
-		verb = verb.strip()
-		matchinverb = check_if_part_of_word1_equals_word2(networkname,verb,len(longestmatch[0]))
-		if len(matchinverb) > 0:
-			longestmatch[0] = matchinverb
-			longestmatch[1] = "verb"
-
-	adjectives = open('28Kadjectives.txt', 'r')
-	for adjective in adjectives:
-		adjective = adjective.strip()
-		matchingadjective = check_if_part_of_word1_equals_word2(networkname,adjective,len(longestmatch[0]))
-		if len(matchingadjective) > 0:
-			longestmatch[0] = matchingadjective
-			longestmatch[1] = "adjective"
-
-	adverbs = open('6Kadverbs.txt', 'r')
-	for adverb in adverbs:
-		adverb = adverb.strip()
-		matchingadverb = check_if_part_of_word1_equals_word2(networkname,adverb,len(longestmatch[0]))
-		if len(matchingadverb) > 0:
-			longestmatch[0] = matchingadverb
-			longestmatch[1] = "adverb"
-
-	obscenes = open('obscene.txt', 'r')
-	for obscene in obscenes:
-		obscene = obscene.strip()
-		matchingadobscene = check_if_part_of_word1_equals_word2(networkname,obscene,len(longestmatch[0]))
-		if len(matchingadobscene) > 0:
-			longestmatch[0] = matchingadobscene
-			longestmatch[1] = "obscene"
-
+	# count = 0
+	for d in dictionaries:
+		dictionairy = open(d + ".txt", 'r')
+		for elem in dictionairy:
+			elem = elem.strip()
+			matchingWord = check_if_part_of_word1_equals_word2(networkname,elem,len(longestmatch[0]))
+			if len(matchingWord) > 0:
+				longestmatch[0] = matchingWord
+				# longestmatch[1] = dictionaries[count][:-1]
+				longestmatch[1] = d[:-1]
+		# count += 1	
 	return longestmatch
+
+
 #------------------------------------------------------------
+# this function expects an array of wordtypes as an input. These are seperated by [space] 
+# and expressed as the singular form of the dictionairy name
+# (e.g. "["people verb people adverb")
 
-
-# networkname = sys.argv[1]
-# print check_for_words_in_network_name(networkname)
-# print time.time() - startTime
-alreadyInspected = set()
-
-import subprocess
-# adapted from here: http://stackoverflow.com/a/4417735
-def run(command):    
-    popen = subprocess.Popen(command, stdout=subprocess.PIPE)
-    return iter(popen.stdout.readline, b"")
-
-#GLOBAL variables:
-current_channel = 1
-def channel_controller(c, c_hop_interval):
-    global current_channel
-    if c > c_hop_interval:
-            idx = channel_range.index(current_channel)
-            idx = idx + 1
-            if idx >= len(channel_range):
-                idx = 0;
-            current_channel = channel_range[idx]
-
-            subprocess.call("airport -c" + str(current_channel), shell=True)
-            # print "///////////////////////////////////////////// Switched to channel: " + str(current_channel)
-            
-            c = 0   
-    else:
-        c = c + 1 
-    return c
-
-
-unused = dict()
-unused["people"] = dict() #network name: word
-unused["noun"] = dict()
-unused["verb"] = dict()
-unused["place"] = dict()
-unused["adjective"] = dict()
-unused["adverb"] = dict()
-unused["obscene"] = dict()
-
-# places adjective people verb people2 adverb obscene
-
+#unused collects networknames and correspinding words that have been found but not used in a senteence yet
+unused = dict() 
+for d in dictionaries:
+	dictName = d[:-1]
+	unused[dictName] = dict()
 
 def returnElements(elementListSpaceDivided):
 	outputList = list()
@@ -161,6 +125,21 @@ def returnElements(elementListSpaceDivided):
 	else:
 		return outputList #test empty returns true
 
+
+#             _       _                                
+#            (_)     | |                               
+#  _ __  _ __ _ _ __ | |_   _ __   ___   ___ _ __ ___  
+# | '_ \| '__| | '_ \| __| | '_ \ / _ \ / _ \ '_ ` _ \ 
+# | |_) | |  | | | | | |_  | |_) | (_) |  __/ | | | | |
+# | .__/|_|  |_|_| |_|\__| | .__/ \___/ \___|_| |_| |_|
+# | |                      | |                         
+# |_|                      |_|                         
+
+# this function expects a list of elements (constructed by the returnElements function) 
+# and a list of integers. The integer pattern list describes the pattern in which the elements
+# are goning to be printed. number describe word on a line, lines are comma seperated
+# eg. "[1,2,1]" means the poem will have 3 line with 1 element on the first line etc.
+# the whole thing will be printed twice, one time with network names, another time with correspinaing words
 def printNewPoem(parts, _pattern):
 	print "-"*30
 	print "\n\n\n\tNew Air Poem:\n\n\n"
@@ -174,85 +153,94 @@ def printNewPoem(parts, _pattern):
 			print ""
 		print ""
 	print "\n\n"
-	# print today.strftime("\t\textracted from air")
-	# print "\t\t\ton " + time.strftime("%A") + ", " + time.strftime("%B") + " " + time.strftime("%d") + " " + time.strftime("%Y")
-	# print "\t\t\tat " + time.strftime("%X")
 	print "\t\textracted from air,"
 	print "\t\t" + time.strftime("%A") + ", " + time.strftime("%B") + " " + time.strftime("%d") + " " + time.strftime("%Y") + ", " + time.strftime("%X")
-
 	print "\n\n"			
 	print "-"*30
 
+
+#                      _                   _   
+#                     | |                 | |  
+#   ___ ___  _ __  ___| |_ _ __ _   _  ___| |_ 
+#  / __/ _ \| '_ \/ __| __| '__| | | |/ __| __|
+# | (_| (_) | | | \__ \ |_| |  | |_| | (__| |_ 
+#  \___\___/|_| |_|___/\__|_|   \__,_|\___|\__|                                                                                         
+#                 _                           
+#                | |                          
+#  ___  ___ _ __ | |_ ___ _ __   ___ ___  ___ 
+# / __|/ _ \ '_ \| __/ _ \ '_ \ / __/ _ \/ __|
+# \__ \  __/ | | | ||  __/ | | | (_|  __/\__ \
+# |___/\___|_| |_|\__\___|_| |_|\___\___||___/
+
 def constructSentence():
-	# people verb people
-	# -
-	# people verb noun
-	# -
-	# place 
-	# adjective people
-	# obscene
-	# - 
-	# adjective people
-	# obscene
-	# -
-	# place 
-	# adjective people
-	# -
-	# adjective people
-	# -
-	# --
-	# place 
-	# adjective noun
-	# obscene
-	# - 
-	# adjective noun
-	# obscene
-	# -
-	# place 
-	# adjective noun
-	# -
-	# adjective noun
-	sentenceParts = returnElements("noun")
+	sentenceParts = returnElements("place people noun")
 	if (len(sentenceParts)):
-		pattern = [1] #describe words per line
+		pattern = [1,2] #describe words per line
 		printNewPoem(sentenceParts, pattern)
 
+	sentenceParts2 = returnElements("place")
+	if (len(sentenceParts2)):
+		pattern = [1] #describe words per line
+		printNewPoem(sentenceParts2, pattern)
 
 
+#            _  __  __      _                   
+#           (_)/ _|/ _|    | |                  
+#  ___ _ __  _| |_| |_     | | ___   ___  _ __  
+# / __| '_ \| |  _|  _|    | |/ _ \ / _ \| '_ \ 
+# \__ \ | | | | | | |      | | (_) | (_) | |_) |
+# |___/_| |_|_|_| |_|      |_|\___/ \___/| .__/ 
+#                                        | |    
+#                                        |_|
 
+# adapted from here: http://stackoverflow.com/a/4417735
+def run(command):    
+    popen = subprocess.Popen(command, stdout=subprocess.PIPE)
+    return iter(popen.stdout.readline, b"")
+
+#GLOBAL variables:
+current_channel = 1
+def channel_controller(c, c_hop_interval):
+    global current_channel
+    if c > c_hop_interval:
+            idx = channel_range.index(current_channel)
+            idx = idx + 1
+            if idx >= len(channel_range):
+                idx = 0;
+            current_channel = channel_range[idx]
+
+            subprocess.call("airport -c" + str(current_channel), shell=True)
+            # print "///////////////////////////////////////////// Switched to channel: " + str(current_channel)
+            c = 0   
+    else:
+        c = c + 1 
+    return c
 
 def sniffloop():
     global current_channel
     count = 10
+    alreadyInspected = set()
     current_channel = channel_range[-1]
     #next line makes all the difference, making sure the hoping works by disassociating fro any network before start:
     # subprocess.call("airport -z", shell=True)		# DEactivate for TESTING
 
     # for line in run("./bps_v1"):		# DEactivate for TESTING
     for line in open("tisch_working_copy.log", "r"):  # activate for TESTING
-		#next line for channel hoping
+		#next line for channel hoping:
 		# count = channel_controller(count, channel_hop_interval)		# DEactivate for TESTING
 		line = line.strip()
-		# print line
 
 		words = line.split(',')
-		# print words
 		if(words[1] == "probe"): # ultimately I'll change the lbtins exec to only show probes (probably)
-            
 			if words[3] not in alreadyInspected:
-				
 				alreadyInspected.add(words[3])
 				returnList = check_for_words_in_network_name(words[3])
 				if(len(returnList[0]) > 0):
 					unused[returnList[1]][words[3]] = returnList[0]
 					# pprint(unused)
-
 		constructSentence()         	
 
-     
-
-       
-        
+           
 
 # GLOBAL variables:
 channel_range = [1,6,11,36,40,44,48]
